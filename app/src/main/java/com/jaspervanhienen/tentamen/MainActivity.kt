2c5eb8f2febf0d4e0f32ef147a7bf67082d41308
Jasper van Hienen <jasperh01@hotmail.com>
2020-03-25 16:39:44 +0100

    Added recyclerview with default values

--- app/src/main/java/com/jaspervanhienen/tentamen/MainActivity.kt
+++ app/src/main/java/com/jaspervanhienen/tentamen/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.jaspervanhienen.tentamen
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        recyclerView_main.adapter = MainAdapter()
     }
 }
