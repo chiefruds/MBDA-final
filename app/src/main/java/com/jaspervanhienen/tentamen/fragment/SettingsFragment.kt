ffda75c05afb2cab0bce6928e4bf3859a208b51a
RUBEN\eigenaar <rbndeheer@gmail.com>
2020-03-30 17:07:23 +0200

    cleanup code

--- app/src/main/java/com/jaspervanhienen/tentamen/fragment/SettingsFragment.kt
+++ app/src/main/java/com/jaspervanhienen/tentamen/fragment/SettingsFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.fragment.app.FragmentActivity
 import androidx.preference.*
 import com.jaspervanhienen.tentamen.R
+import kotlin.reflect.typeOf
 
 
 class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
@@ -29,46 +30,64 @@ class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedP
     private fun updateSummary(p: Preference): String {
         if(context == null)
             context = activity
+
         val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
             p.summary = ""
-        //Log.d("preference", "p" + preferences.all)
-        if (p is EditTextPreference) {
-            p.setSummary(preferences.getString(p.getKey(), ""))
-        } else if (p is ListPreference) {
-            val value = preferences.getString(p.getKey(), "")
+
+        when(p) {
+            is EditTextPreference -> updateEditText(p, preferences)
+            is ListPreference -> updateListPreference(p, preferences)
+            is MultiSelectListPreference -> updateMultiSelectList(p, preferences)
+            is PreferenceCategory -> updateCategory(p)
+            is PreferenceGroup -> updateGroup(p)
+        }
+
+        return p.summary.toString()
+    }
+
+
+    private fun updateEditText(p: Preference, preferences : SharedPreferences) {
+        p.summary = preferences.getString(p.key, "")
+    }
+
+    //when list, check if not empty and set summary
+    private fun updateListPreference(p: ListPreference, preferences: SharedPreferences) {
+        val value = preferences.getString(p.key, "")
+        val index = p.findIndexOfValue(value)
+        if (index >= 0) {
+            p.summary = p.entries[index]
+        }
+    }
+
+    //when multiselect list, retrieve values, loop over values and update preference summary
+    private fun updateMultiSelectList(p: MultiSelectListPreference, preferences : SharedPreferences) {
+        val values = preferences.getStringSet(
+            p.key,
+            emptySet()
+        )
+        for (value in values!!) {
             val index: Int = (p).findIndexOfValue(value)
             if (index >= 0) {
-                p.setSummary((p).entries[index])
-            }
-        } else if (p is MultiSelectListPreference) {
-            val values = preferences.getStringSet(
-                p.getKey(),
-                emptySet()
-            )
-            for (value in values!!) {
-                val index: Int = (p).findIndexOfValue(value)
-                if (index >= 0) {
-                    p.setSummary(
-                        p.getSummary().toString() + (if (p.getSummary().isEmpty()
-                        ) "" else ", ") + (p).entries.get(index)
-                    )
-                }
-            }
-        } else if (p is PreferenceCategory) {
-            val preference: PreferenceCategory = p
-            for (i in 0 until preference.preferenceCount) {
-                updateSummary(preference.getPreference(i))
-            }
-        } else if (p is PreferenceGroup) {
-            val preference: PreferenceGroup = p
-            for (i in 0 until preference.preferenceCount) {
-                p.setSummary(
-                    p.getSummary().toString() + (if (p.getSummary().isEmpty()
-                    ) "" else ", ") + updateSummary(preference.getPreference(i))
-                )
+                p.summary = p.summary.toString() + (if (p.summary.isEmpty()
+                ) "" else ", ") + (p).entries[index]
             }
         }
-       // Log.d("summary", p.summary.toString())
-        return p.summary.toString()
+    }
+
+    //when group, loop over all preferences and update summary
+    private fun updateGroup(p: PreferenceGroup) {
+        val preference: PreferenceGroup = p
+        for (i in 0 until preference.preferenceCount) {
+            p.summary = p.summary.toString() + (if (p.summary.isEmpty()
+            ) "" else ", ") + updateSummary(preference.getPreference(i))
+        }
+    }
+
+    //when category, update summary for each preference
+    private fun updateCategory(p: PreferenceCategory) {
+        val preference: PreferenceCategory = p
+        for (i in 0 until preference.preferenceCount) {
+            updateSummary(preference.getPreference(i))
+        }
     }
 }
